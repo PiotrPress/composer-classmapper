597bff028dd69caaa04eb00b5f3e07eb292d7777
PiotrPress <admin@piotr.press>
2024-03-27 16:47:14 +0100

    v3.1.0

--- res/autoload.php
+++ res/autoload.php
@@ -1,3 +1,3 @@
-<?php return spl_autoload_register( function( string $class ) : void {
-    if( ( $map = require __DIR__ . '/classmap.php' )[ $class ] ?? null ) require __DIR__ . $map[ $class ];
-} ); # This file is part of https://github.com/PiotrPress/composer-classmapper package.
+<?php return ( function() { $map = require __DIR__ . '/classmap.php';
+    return spl_autoload_register( fn( string $class ) => ! ( $map[ $class ] ?? null ) ?: require __DIR__ . $map[ $class ] );
+} )(); # This file is part of https://github.com/PiotrPress/composer-classmapper package.
